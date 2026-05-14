8f3d019c74b79040495507c60efbb7e76a8a05f6
Prvncodz <praveenwks@gmail.com>
2026-05-14 17:18:10 +0530

    added apierror class

---
+++ server/src/utils/ErrorHandler.js
@@ -0,0 +1,25 @@
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        name,
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+        (this.statusCode = statusCode,
+            this.data = null,
+            this.name = name,
+            this.errors = errors,
+            this.message = message,
+            this.success = false
+        );
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+export default ApiError
