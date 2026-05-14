3e68da3a068b6075c6a3d0c4e9d442e44477f940
Prvncodz <praveenwks@gmail.com>
2026-05-14 19:53:34 +0530

    added generates token methods and ispasscorrect method to user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose"
-
+import bycrypt from "bycrypt"
 
 const UserSchema = new Schema({
     avatar: {
@@ -34,4 +34,46 @@ const UserSchema = new Schema({
 }, { timestamps: true })
 
 const User = mongoose.model('User', UserSchema)
+
+User.pre('save', async (next) => {
+    if (this.isModified("password")) {
+        this.password = await bycrypt.hash(this.password, 10)
+        return next()
+    } else {
+        return next()
+    }
+});
+
+User.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            fullname: this.fullname,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+
+User.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            fullname: this.fullname,
+            email: this.email
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
+
+User.methods.isPasswordCorrect = async function (password) {
+    return await bycrypt.compare(password, this.password)
+}
+
 export default User
