668be3a3af37e44748e8039672566d5c71c1c216
Prvncodz <praveenwks@gmail.com>
2026-05-15 08:28:37 +0530

    added built-in middleware to accept input & other stuff

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,15 @@
+import cookieParser from "cookie-parser";
 import express from "express"
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "30kb" }));
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true, limit: "30kb" }))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200,// some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true
+}))
+app.use(cookieParser());
 
 export default app;
