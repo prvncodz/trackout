19bcd2a981055948a820fece174a0e41ce1833ee
Prvncodz <praveenwks@gmail.com>
2026-05-15 09:43:37 +0530

    added cloudinary methods and controllers

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,57 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { file } from "zod";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const UploadToCloud = async (filepath) => {
+    try {
+        if (!filepath) {
+            return null
+        }
+        if (!fs.existsSync(filepath)) {
+            console.log("file doesn't exist")
+            return null;
+        }
+        console.log("uploading file to the cloud...")
+
+        const res = await cloudinary.uploader.upload(filepath, {
+            resource_type: 'auto'
+        });
+
+        if (res) {
+            console.log("file has been uploaded successfully")
+        }
+        fs.unlinkSync(filepath)
+        return res
+
+    } catch (error) {
+        fs.unlinkSync(filepath)
+        console.error(error.message)
+        return null
+    }
+
+}
+
+const DeleteFromCloud = async (public_id) => {
+    if (!public_id) {
+        return null;
+    }
+    try {
+        console.log("deleting file from cloud...")
+        await cloudinary.uploader.destroy(public_id)
+        return true;
+    } catch (error) {
+        console.log("failed to delete file from cloud")
+        console.error(error.message)
+        return null
+    }
+}
+
+export { UploadToCloud }
+
