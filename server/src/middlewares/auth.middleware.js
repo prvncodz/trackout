b19bbef16464b29d18de3c5ab405b6027a826e0b
Prvncodz <praveenwks@gmail.com>
2026-05-16 11:15:37 +0530

    added logout endpoint

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import ApiError from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"
+
+const auth = asyncHandler(async (req, res, next) => {
+    const token = req?.cookies?.accessToken || req?.headers['authorization']?.split(" ")?.[1]
+    if (!token) {
+        throw new ApiError(401, "unauthorized request")
+    }
+    const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    if (!decodedToken) {
+        throw new ApiError(401, "token expired")
+    }
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+        throw new ApiError(404, "user not found")
+    }
+    req.user = user
+    next();
+})
+
+export default auth;
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { SignInUser, SignUpUser, LogOutUser } from "../controllers/user.controllers.js";
 import express from "express"
-
+import auth from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 
 router.post("/signup", SignUpUser);
 router.post("/signin", SignInUser);
-router.post("/logout", LogOutUser);
+router.post("/logout", auth, LogOutUser);
 
 export default router;
