f585f87cf0dd7eceb325db2f7e83fc81677d778d
Prvncodz <praveenwks@gmail.com>
2026-05-21 18:54:59 +0530

    made activity model in order to track total active days,active dates and consistency streak of user

---
+++ server/src/models/activity.model.js
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+const ActivitySchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, "owner is required"]
+    },
+}, { timestamps: true })
+
+const Activity = mongoose.model('Activity', ActivitySchema)
+export default Activity
