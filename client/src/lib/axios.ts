5156a9aa4116591fe79ae3d61c3db454ed8d52d5
Prvncodz <praveenwks@gmail.com>
2026-06-08 11:01:52 +0530

    fix(ui): calender

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -5,17 +5,5 @@ const api = axios.create({
     baseURL: "http://localhost:8000/api/v1",
 })
 
-api.interceptors.response.use(
-    res => res,
-    async (error) => {
-        const status = error.response ? error.response.status : null;
-
-        if (status === 401) {
-            await api.get("/user/refresh-tokens")
-        }
-
-        return Promise.reject(error);
-    }
-);
 
 export default api
