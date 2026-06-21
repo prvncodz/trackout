768cb969a9e92dd642376352e8790d845e15ac9b
Prvncodz <praveenwks@gmail.com>
2026-06-21 21:08:03 +0530

    fix(ui):dashboard stuff

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -159,7 +159,7 @@ const DashboardPage = () => {
                             <ChartAreaInteractive />
                         </div>
                     </div>
-                    <div className="flex h-screen flex-col gap-10 md:flex-row md:items-start lg:flex-col lg:px-0">
+                    <div className="flex h-screen flex-col gap-10 px-5 md:flex-row md:items-start lg:flex-col lg:px-0">
                         <div className="w-full shrink-0 overflow-hidden rounded-2xl border border-neutral-200 bg-white p-5 md:w-80 lg:w-120 dark:border-neutral-800 dark:bg-neutral-900 dark:text-line-color">
                             <Calendar size="md" className="w-full bg-white  rounded-2xl md:w-auto lg:w-full dark:bg-neutral-900 dark:text-line-color " />
                         </div>
